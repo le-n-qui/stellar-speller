22a281de343452f4ad82207e4b68322ce673d8b2
Andy Le <andy.quile@gmail.com>
2021-11-30 23:14:52 -0800

    This is the current version of addNode and lookup methods; still need to debug to pass unit tests

--- src/cs146F21/lenaqvi/project4/RedBlackTree.java
+++ src/cs146F21/lenaqvi/project4/RedBlackTree.java
@@ -30,6 +30,12 @@ public class RedBlackTree {
 	  		}
 	  		return false;
 	  	}
+	  	
+	  	public String getKey() {
+	  		if (this == null)
+	  			return "";
+	  		return key;
+	  	}
 	}
 
 	public boolean isLeaf(RedBlackTree.Node n){
@@ -69,8 +75,51 @@ public class RedBlackTree {
 	
 	// place a new node in the RB tree with data the parameter and color it red. 
 	public void addNode(String data){  	//this < that  <0.  this > that  >0
-	 //	fill
-		
+		// Create a new RBTree node
+		RedBlackTree.Node newNode = new RedBlackTree.Node(data);
+		// Base case
+		if (root == null) {
+			root = newNode;
+			newNode.parent = null;
+		 	newNode.isRed = false;
+		 	newNode.color = 1;
+		}
+		else {
+			newNode.color = 0;
+		 	newNode.isRed = true;
+			// Have current node point at the root node
+			RedBlackTree.Node currNode = root; 
+			// Traverse and find the right location for new node
+			while (currNode != null) {
+				// if value of new node is greater than that of current node
+				if (newNode.compareTo(currNode) > 0) {
+					if (currNode.rightChild == null) {
+						currNode.rightChild = newNode;
+						newNode.parent = currNode;
+						break;
+					}
+		 			else
+		 				// Proceed to the right subtree
+		 				currNode = currNode.rightChild;
+				}
+				// if value of new node is less than that of current node
+				else if (newNode.compareTo(currNode) < 0) {
+					if (currNode.leftChild == null) {
+						currNode.leftChild = newNode;
+						newNode.parent = currNode;
+						break;
+					}
+					else
+						// Proceed to the left subtree
+						currNode = currNode.leftChild;
+				}
+				// if value of new node is similar to that of current node
+				//else {
+					// Place new node to the right of current node
+				//	currNode.rightChild = newNode;
+				//}
+			}
+		}
 	}	
 
 	public void insert(String data){
@@ -79,17 +128,44 @@ public class RedBlackTree {
 	
 	public RedBlackTree.Node lookup(String k){ 
 		//fill
+		RedBlackTree.Node currNode = root;
+	    
+	    //creating target Nod with given data
+	    RedBlackTree.Node targetNode = new RedBlackTree.Node(k);
+	    if (root == null)
+	      return null;
+
+	    //iterate while the current node isnt null
+	    while (currNode != null) {
+	      if (currNode.compareTo(targetNode) > 0) {
+	    	 
+	    	  currNode = currNode.rightChild;
+	           
+	      }
+	      else if (currNode.compareTo(targetNode) < 0) {
+	        
+	          currNode = currNode.leftChild;
+	            
+	      }
+	      else
+	        // equal
+	        break;
+	    }
+	    
+	    //if (currNode == null)
+	    	
+	    return currNode;
 	}
  	
 	
-	public RedBlackTree.Node getSibling(RedBlackTree.Node n){  
+	//public RedBlackTree.Node getSibling(RedBlackTree.Node n){  
 		//
-	}
+	//}
 	
 	
-	public RedBlackTree.Node getAunt(RedBlackTree.Node n){
+	//public RedBlackTree.Node getAunt(RedBlackTree.Node n){
 		//
-	}
+	//}
 	
 	public RedBlackTree.Node getGrandparent(RedBlackTree.Node n){
 		return n.parent.parent;
