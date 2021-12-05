c8a783600b8705eb8e6faff8d0d1b07e322e2d9f
Mohammad Naqvi <mohammad.naqvi@sjsu.edu>
2021-12-05 11:54:06 -0800

    Modified makeStringDetails

--- src/cs146F21/lenaqvi/project4/RBTTester.java
+++ src/cs146F21/lenaqvi/project4/RBTTester.java
@@ -7,7 +7,38 @@ import org.junit.jupiter.api.Test;
 class RBTTester {
 
 	@Test
-	void test() {
+	//Test the Red Black Tree
+	// Dr. Potika Test
+	public void test() {
+		RedBlackTree rbt = new RedBlackTree();
+		rbt.insert("D");
+		rbt.insert("B");
+		rbt.insert("A");
+		rbt.insert("C");
+		rbt.insert("F");
+		rbt.insert("E");
+		rbt.insert("H");
+		rbt.insert("G");
+		rbt.insert("I");
+		rbt.insert("J");
+		assertEquals("DBACFEHGIJ", makeString(rbt));
+
+		//final tree product
+		String str=     "Color: 1, Key:D Parent: \n"+
+						"Color: 1, Key:B Parent: D\n"+
+						"Color: 1, Key:A Parent: B\n"+
+						"Color: 1, Key:C Parent: B\n"+
+						"Color: 1, Key:F Parent: D\n"+
+						"Color: 1, Key:E Parent: F\n"+
+						"Color: 0, Key:H Parent: F\n"+
+						"Color: 1, Key:G Parent: H\n"+
+						"Color: 1, Key:I Parent: H\n"+
+						"Color: 0, Key:J Parent: I\n";
+		assertEquals(str, makeStringDetails(rbt));
+	}
+
+	@Test
+	void addTest() {
 		RedBlackTree rbt = new RedBlackTree();
         rbt.insert("D");
         rbt.insert("B");
@@ -15,23 +46,112 @@ class RBTTester {
         rbt.insert("C");
         rbt.insert("F");
         rbt.insert("E");
-        rbt.insert("H");
-        rbt.insert("G");
-        rbt.insert("I");
-        rbt.insert("J");
-        assertEquals("DBACFEHGIJ", makeString(rbt));
-        String str=     "Color: 1, Key:D Parent: \n"+
-                        "Color: 1, Key:B Parent: D\n"+
-                        "Color: 1, Key:A Parent: B\n"+
-                        "Color: 1, Key:C Parent: B\n"+
-                        "Color: 1, Key:F Parent: D\n"+
-                        "Color: 1, Key:E Parent: F\n"+
-                        "Color: 0, Key:H Parent: F\n"+
-                        "Color: 1, Key:G Parent: H\n"+
-                        "Color: 1, Key:I Parent: H\n"+
-                        "Color: 0, Key:J Parent: I\n";
-		assertEquals(str, makeStringDetails(rbt));
+        System.out.println(makeString(rbt));
+        assertEquals("BADCFE", makeString(rbt));
             
+	}
+
+	@Test
+	void lookUpTest() {
+		RedBlackTree rbt = new RedBlackTree();
+		rbt.insert("D");
+		rbt.insert("B");
+		rbt.insert("A");
+		rbt.insert("C");
+		rbt.insert("F");
+		rbt.insert("E");
+
+		RedBlackTree.Node result = rbt.lookup("A");
+		assertNotNull(result);
+	}
+
+	@Test
+	void getSiblingTest() {
+		RedBlackTree rbt = new RedBlackTree();
+		rbt.insert("D");
+		rbt.insert("B");
+		rbt.insert("A");
+		rbt.insert("C");
+		rbt.insert("F");
+		rbt.insert("E");
+		rbt.insert("H");
+		rbt.insert("G");
+		rbt.insert("I");
+		rbt.insert("J");
+
+		RedBlackTree.Node sibling_A = rbt.getSibling(rbt.lookup("A"));
+		assertEquals(sibling_A.key, "C");
+
+		RedBlackTree.Node sibling_F = rbt.getSibling(rbt.lookup("F"));
+		assertEquals(sibling_F.key, "B");
+
+		RedBlackTree.Node sibling_J = rbt.getSibling(rbt.lookup("J"));
+		//can't call key on a null pointer
+		assertNull(sibling_J);
+	}
+
+	@Test
+	void getAuntTest() {
+		RedBlackTree rbt = new RedBlackTree();
+		rbt.insert("D");
+		rbt.insert("B");
+		rbt.insert("A");
+		rbt.insert("C");
+		rbt.insert("F");
+		rbt.insert("E");
+		rbt.insert("H");
+		rbt.insert("G");
+		rbt.insert("I");
+		rbt.insert("J");
+
+		RedBlackTree.Node aunt_A = rbt.getAunt(rbt.lookup("A"));
+		assertEquals(aunt_A.key, "F");
+
+		RedBlackTree.Node aunt_H = rbt.getAunt(rbt.lookup("H"));
+		assertEquals(aunt_H.key, "B");
+
+		RedBlackTree.Node aunt_J = rbt.getAunt(rbt.lookup("J"));
+		assertEquals(aunt_J.key, "G");
+
+		RedBlackTree.Node aunt_F = rbt.getAunt(rbt.lookup("F"));
+		//can't call key on a null pointer
+		assertNull(aunt_F);
+	}
+
+	@Test
+	void rotateLeftTest() {
+		RedBlackTree rbt = new RedBlackTree();
+		rbt.insert("D");
+		rbt.insert("B");
+		rbt.insert("F");
+		rbt.insert("E");
+		rbt.insert("I");
+		rbt.insert("H");
+		rbt.insert("G");
+
+		rbt.rotateLeft(rbt.lookup("F"));
+		RedBlackTree.Node node_F = rbt.lookup("F");
+		assertEquals(node_F.parent.key, "H");
+		assertEquals(node_F.rightChild.key, "G");
+	}
+
+
+
+	@Test
+	void rotateRightTest() {
+		RedBlackTree rbt = new RedBlackTree();
+		rbt.insert("D");
+		rbt.insert("B");
+		rbt.insert("F");
+		rbt.insert("E");
+		rbt.insert("I");
+		rbt.insert("H");
+		rbt.insert("G");
+
+		rbt.rotateRight(rbt.lookup("F"));
+		RedBlackTree.Node node_F = rbt.lookup("F");
+		assertEquals(node_F.parent.key, "E");
+		assertEquals(node_F.rightChild.key, "H");
 	}
 	
 	//add tester for spell checker
@@ -55,7 +175,9 @@ class RBTTester {
     	class MyVisitor implements RedBlackTree.Visitor {
     		String result = "";
     		public void visit(RedBlackTree.Node n) {
-    			if(!(n.key).equals(""))
+				if (n.parent == null)
+					result = result + "Color: "+n.color+", Key:"+n.key+" Parent: \n";
+    			else if (!(n.key).equals(""))
     				result = result +"Color: "+n.color+", Key:"+n.key+" Parent: "+n.parent.key+"\n";
              
     		}
--- src/cs146F21/lenaqvi/project4/RedBlackTree.java
+++ src/cs146F21/lenaqvi/project4/RedBlackTree.java
@@ -1,63 +1,85 @@
 package cs146F21.lenaqvi.project4;
 
-public class RedBlackTree {	
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RedBlackTree {
 	private RedBlackTree.Node root;
-    
-	public static class Node {
-		
-		String key;  		  
-	  	Node parent;
-	  	Node leftChild;
-	  	Node rightChild;
-	  	boolean isRed;
-	  	int color;
-	  
-	  	public Node(String data){
-	  		this.key = data;
-	  		leftChild = null;
-	  		rightChild = null;
-	  	}		
-	  
-	  	public int compareTo(Node n) { 		//this < that  < 0
-	  		return key.compareTo(n.key);  	//this > that  > 0
-	  	}
-	  
-	  	public boolean isLeaf(){
-	  		if (this.equals(root) && this.leftChild == null && this.rightChild == null) return true;
-	  		if (this.equals(root)) return false;
-	  		if (this.leftChild == null && this.rightChild == null){
-	  			return true;
-	  		}
-	  		return false;
-	  	}
+
+	public static void main(String[] args) {
+		RedBlackTree rbt = new RedBlackTree();
+		rbt.insert("D");
+		rbt.insert("B");
+		rbt.insert("A");
+		rbt.insert("C");
+		rbt.insert("F");
+		rbt.insert("E");
+		rbt.insert("H");
+		rbt.insert("G");
+		rbt.insert("I");
+		rbt.insert("J");
+		System.out.println(RBTTester.makeString(rbt));
+		System.out.println(RBTTester.makeStringDetails(rbt));
+
+	}
+	public class Node {
+
+		String key;
+		Node parent;
+		Node leftChild;
+		Node rightChild;
+		boolean isRed;
+		int color;
+
+		public Node(String data){
+			this.key = data;
+			leftChild = null;
+			rightChild = null;
+		}
+
+		public int compareTo(Node n){ 	//this < that  <0
+			return key.compareTo(n.key);  	//this > that  >0
+		}
+
+		public boolean isLeaf(){
+			if (this.equals(root) && this.leftChild == null && this.rightChild == null) return true;
+			if (this.equals(root)) return false;
+			if (this.leftChild == null && this.rightChild == null){
+				return true;
+			}
+			return false;
+		}
+
+		public String getKey() {
+			return key;
+		}
 	}
 
-	public boolean isLeaf(RedBlackTree.Node n){
+	public boolean isLeaf(RedBlackTree.Node n) {
 		if (n.equals(root) && n.leftChild == null && n.rightChild == null) return true;
 		if (n.equals(root)) return false;
 		if (n.leftChild == null && n.rightChild == null){
-			  return true;
-		  }
-		  return false;
+			return true;
+		}
+		return false;
 	}
-	
+
 	public interface Visitor{
 		/**
-		This method is called at each node.
-		@param n the visited node
-		*/
-		void visit(Node n);  
+		 This method is called at each node.
+		 @param n the visited node
+		 */
+		void visit(Node n);
 	}
-	
+
 	public void visit(Node n){
 		System.out.println(n.key);
 	}
-	
+
 	public void printTree(){  //preorder: visit, go left, go right
-		RedBlackTree.Node currentNode = root;	
+		RedBlackTree.Node currentNode = root;
 		printTree(currentNode);
 	}
-	
+
 	public void printTree(RedBlackTree.Node node){
 		System.out.print(node.key);
 		if (node.isLeaf()){
@@ -66,91 +88,266 @@ public class RedBlackTree {
 		printTree(node.leftChild);
 		printTree(node.rightChild);
 	}
-	
-	// place a new node in the RB tree with data the parameter and color it red. 
-	public void addNode(String data){  	//this < that  <0.  this > that  >0
-	// creates a RBT Node w/ specified data
-	 RedBlackTree.Node newNode = new RedBlackTree.Node(data);
 
-	 if (root == null) {
-	 	root = newNode;
-	 	newNode.parent = null;
-	 	newNode.isRed = false;
-	 	newNode.color = 1; //assuming '1' is black
-	 }
-	 else {
+	// place a new node in the RB tree with data the parameter and color it red.
+	public void addNode(String data) {
 
-	 	newNode.color = 0;
-	 	newNode.isRed = true;
-	 	//start iteration from the root
-	 	RedBlackTree.Node curr = root;
+		// Create a new RBTree node
+		RedBlackTree.Node newNode = new RedBlackTree.Node(data);
+		// Base case
+		if (root == null) {
 
-	 	while(curr != null) {
+			root = newNode;
+			newNode.parent = null;
+			newNode.isRed = false;
+			newNode.color = 1;
 
-	 		if (newNode.compareTo(curr) > 0) {
-	 			if (curr.rightChild == null) {
-					curr.rightChild = newNode;
-					newNode.parent = curr;
-					break;
+		}
+		else {
+			// if root is not null
+			// we want new node to be red originally
+			newNode.color = 0;
+			newNode.isRed = true;
+			// Have current node point at the root node
+			RedBlackTree.Node currNode = root;
+			// Traverse and find the right location for new node
+			while (currNode != null) {
+				// if value of new node is greater than that of current node
+
+				// if value of new node is less than that of current node
+
+				if (newNode.compareTo(currNode) > 0) {
+					if (currNode.rightChild == null) {
+						newNode.parent = currNode;
+						currNode.rightChild = newNode;
+
+						break;
+					}
+					// Proceed to the right subtree
+					currNode = currNode.rightChild;
+
 				}
-	 			else
-	 				curr = curr.rightChild;
-			}
-			if (newNode.compareTo(curr) < 0) {
-				if (curr.leftChild == null) {
-					curr.leftChild = newNode;
-					newNode.parent = curr;
-					break;
+				else if (newNode.compareTo(currNode) < 0) {
+					if (currNode.leftChild == null) {
+						newNode.parent = currNode;
+						currNode.leftChild = newNode;
+
+						break;
+					}
+					// Proceed to the left subtree
+					currNode = currNode.leftChild;
 				}
-				else
-					curr = curr.leftChild;
+				// if value of new node is similar to that of current node
+				//else {
+				// Place new node to the right of current node
+				//	currNode.rightChild = newNode;
+				//}
 			}
 		}
-	 }
-	 //this is where we call fixtree
-	}	
-
-	public void insert(String data) {
-		addNode(data);	
+		fixTree(newNode);
 	}
-	
+
+	/**
+	 * insert method calls addNode method
+	 * to add new node to the Red Black tree.
+	 * @param data
+	 */
+	public void insert(String data){
+		addNode(data);
+	}
+
+	/**
+	 * lookup method searches for
+	 * the node with desired string key, k.
+	 * @param k
+	 * @return reference to the node with string k.
+	 */
 	public RedBlackTree.Node lookup(String k) {
 		//fill
+		RedBlackTree.Node currNode = root;
+
+		//creating target Nod with given data
+		RedBlackTree.Node targetNode = new RedBlackTree.Node(k);
+		if (root == null)
+			return null;
+
+		//iterate while the current node isn't null
+		while (currNode != null) {
+			if (targetNode.compareTo(currNode) > 0) {
+
+				currNode = currNode.rightChild;
+
+			}
+			else if (targetNode.compareTo(currNode) < 0) {
+
+				currNode = currNode.leftChild;
+
+			}
+			else
+				// equal
+				break;
+		}
+
+		//if (currNode == null)
+
+		return currNode;
 	}
- 	
-	
-	public RedBlackTree.Node getSibling(RedBlackTree.Node n) {
-		//
+
+
+	/**
+	 * getSibling method retrieves the node
+	 * that is on the same level as the argument node, n.
+	 * @param n
+	 * @return reference to the sibling node
+	 */
+	public RedBlackTree.Node getSibling(RedBlackTree.Node n){
+		if (n.parent != null) {
+			if (n.parent.leftChild != null && !isLeftChild(n.parent,n))
+				return n.parent.leftChild;
+			else {
+				if(n.parent.rightChild != null && isLeftChild(n.parent, n))
+					return n.parent.rightChild;
+			}
+		}
+		return null;
 	}
-	
-	
-	public RedBlackTree.Node getAunt(RedBlackTree.Node n) {
-		//
+
+
+	public RedBlackTree.Node getAunt(RedBlackTree.Node n){
+		if (n.parent == null)
+			return null;
+		return getSibling(n.parent);
 	}
-	
-	public RedBlackTree.Node getGrandparent(RedBlackTree.Node n) {
+
+	public RedBlackTree.Node getGrandparent(RedBlackTree.Node n){
+		if (n.parent.parent == null)
+			return null;
 		return n.parent.parent;
 	}
-	
-	public void rotateLeft(RedBlackTree.Node n) {
-		//
+
+	public void rotateLeft(RedBlackTree.Node n){
+		// Set a reference to the right subtree of n
+		RedBlackTree.Node tempRightChild = n.rightChild;
+		n.rightChild = tempRightChild.leftChild;
+
+		if (tempRightChild.leftChild != null) {
+			tempRightChild.leftChild.parent = n;
+		}
+
+		tempRightChild.parent = n.parent;
+
+		if (n.parent == null) {
+			root = tempRightChild;
+		}
+		else if (n == n.parent.leftChild) {
+			n.parent.leftChild = tempRightChild;
+		}
+		else {
+			n.parent.rightChild = tempRightChild;
+		}
+
+		tempRightChild.leftChild = n;
+		n.parent = tempRightChild;
 	}
-	
-	public void rotateRight(RedBlackTree.Node n) {
-		//
+
+	public void rotateRight(RedBlackTree.Node n){
+		// Set a reference to the left subtree of n
+		RedBlackTree.Node tempLeftChild = n.leftChild;
+		n.leftChild = tempLeftChild.rightChild;
+
+		if (tempLeftChild.rightChild != null) {
+			tempLeftChild.rightChild.parent = n;
+		}
+
+		tempLeftChild.parent = n.parent;
+
+		if (n.parent == null) {
+			root = tempLeftChild;
+		}
+		else if (n == n.parent.rightChild) {
+			n.parent.rightChild = tempLeftChild;
+		}
+		else {
+			n.parent.leftChild = tempLeftChild;
+		}
+
+		tempLeftChild.rightChild = n;
+		n.parent = tempLeftChild;
 	}
-	
+
+
 	public void fixTree(RedBlackTree.Node current) {
-		//
+		// case 1
+		// case 1
+		if (current == root) {
+			current.color = 1;
+			current.isRed = false;
+			return;
+		}
+		if (current.parent.color == 1) {
+			return;
+		}
+
+		if (current.isRed == true && current.parent.isRed == true)
+		{
+
+
+			if (getAunt(current) == null || getAunt(current).isRed == false)
+			{
+
+
+				if (getGrandparent(current).leftChild == current.parent && current == current.parent.rightChild) {
+					rotateLeft(current.parent);
+					RedBlackTree.Node originalParent = current.parent;
+					fixTree(originalParent);
+				}
+				else if (getGrandparent(current).rightChild == current.parent && current == current.parent.leftChild) {
+					rotateRight(current.parent);
+					RedBlackTree.Node originalParent = current.parent;
+					fixTree(originalParent);
+				}
+				else if (getGrandparent(current).leftChild == current.parent && current == current.parent.leftChild) {
+					current.parent.color = 1;
+					current.parent.isRed = false;
+					getGrandparent(current).color = 0;
+					getGrandparent(current).isRed = true;
+					rotateRight(getGrandparent(current));
+					return;
+				}
+
+				else if (getGrandparent(current).rightChild == current.parent && current == current.parent.rightChild) {
+					current.parent.color = 1;
+					current.parent.isRed = false;
+					getGrandparent(current).color = 0;
+					getGrandparent(current).isRed = true;
+					rotateLeft(getGrandparent(current));
+					return;
+				}
+			}
+
+			else {
+				if (getAunt(current).isRed == true) {
+					current.parent.color = 1;
+					current.parent.isRed = false;
+					getAunt(current).color = 1;
+					getAunt(current).isRed = false;
+					getGrandparent(current).color = 0;
+					getGrandparent(current).isRed = true;
+					fixTree(getGrandparent(current));
+				}
+			}
+		}
 	}
-	
+
+
+
 	public boolean isEmpty(RedBlackTree.Node n){
 		if (n.key == null){
 			return true;
 		}
 		return false;
 	}
-	 
+
 	public boolean isLeftChild(RedBlackTree.Node parent, RedBlackTree.Node child)
 	{
 		if (child.compareTo(parent) < 0 ) {//child is less than parent
@@ -160,17 +357,16 @@ public class RedBlackTree {
 	}
 
 	public void preOrderVisit(Visitor v) {
-	   	preOrderVisit(root, v);
+		preOrderVisit(root, v);
 	}
-	 
-	 
-	private static void preOrderVisit(RedBlackTree.Node n, Visitor v) {
-	  	if (n == null) {
-	  		return;
-	  	}
-	  	v.visit(n);
-	  	preOrderVisit(n.leftChild, v);
-	  	preOrderVisit(n.rightChild, v);
-	}	
-}
 
+
+	private static void preOrderVisit(RedBlackTree.Node n, Visitor v) {
+		if (n == null) {
+			return;
+		}
+		v.visit(n);
+		preOrderVisit(n.leftChild, v);
+		preOrderVisit(n.rightChild, v);
+	}
+}
